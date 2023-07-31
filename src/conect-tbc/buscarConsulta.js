7a94f45de44f04d17c30dad3551dd374680de370
unknown <joaovitormirandabandeira@gmail.com>
2023-07-31 08:10:01 -0300

    subindo a api de buscar consulta

--- src/conect-tbc/buscarConsulta.js
+++ src/conect-tbc/buscarConsulta.js
@@ -1,4 +1,4 @@
-function buscarConsulta(url, user, passWord,codColigada,codSistema,codSentenca,codFilial) {
+function buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenca, codFilial) {
     const https = require('https')
     const http = require('http'); // Use the 'https' module instead of 'http'
     const x2jsrequire = require('x2js');
@@ -10,7 +10,7 @@ function buscarConsulta(url, user, passWord,codColigada,codSistema,codSentenca,c
         headers: {
             Authorization: 'Basic ' + Buffer.from(`${user}:${passWord}`).toString('base64'),
             'Content-Type': 'text/xml',
-            SOAPAction: `${url}/wsDataServer/IwsDataServer`
+            SOAPAction: `http://www.totvs.com/IwsDataServer/ReadRecord`
         }
     };
     const soapRequestWithTimeout = (url, config, xml) => {
@@ -45,15 +45,42 @@ function buscarConsulta(url, user, passWord,codColigada,codSistema,codSentenca,c
           <tot:Contexto>codcoligada=${codColigada};codfilial=${codFilial};codsistema=${codSistema};codusuario=${user}</tot:Contexto>
        </tot:ReadRecord>
     </soapenv:Body>
- </soapenv:Envelope>`;
-
+ </soapenv:Envelope>`
+ /*`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <tot:ReadRecord>
+         <!--Optional:-->
+         <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
+         <!--Optional:-->
+         <tot:PrimaryKey>0;S;RB.PS.IM.010</tot:PrimaryKey>
+         <!--Optional:-->
+         <tot:Contexto>codcoligada=1;codfilial=1;codsistema=s;codusuario=rubeus</tot:Contexto>
+      </tot:ReadRecord>
+   </soapenv:Body>
+</soapenv:Envelope>`*/;
+    /*const myAction = async () => {
+        try {
+          const response = await soapRequestWithTimeout(url, config, xml);
+          if (response.status === 200) {
+            const sentença = JSON.parse(response.Envelope.Body.ReadRecordResponse.ReadRecordResult);
+            return sentença;
+          } else {
+            return 'Erro na solicitação SOAP:' + response.status;
+          }
+        } catch (error) {
+          return 'Erro na solicitação SOAP:' + error;
+        }
+      };*/
     const myAction = async () => {
         try {
             const response = await soapRequestWithTimeout(url, config, xml);
             const json = x2js.xml2js(response);
-            return json.Envelope.Body
+            const stringXML = json.Envelope.Body.ReadRecordResponse.ReadRecordResult
+            const sentence = x2js.xml2js(stringXML).GlbConsSql.GConsSql
+            return sentence
         } catch (error) {
-            return 'Erro na solicitação SOAP:' + error
+            return 'Erro na solicitação SOAP:' + error;
         }
     };
     return myAction();
