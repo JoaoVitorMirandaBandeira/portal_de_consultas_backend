3d576ecc0c6c568815e01471accdc4dd98cfd41d
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-07-31 18:52:49 -0300

    Concluido a api de salvar consulta

--- src/conect-tbc/buscarConsulta.js
+++ src/conect-tbc/buscarConsulta.js
@@ -1,9 +1,24 @@
-function buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenca, codFilial) {
+function buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenca) {
     const https = require('https')
     const http = require('http'); // Use the 'https' module instead of 'http'
     const x2jsrequire = require('x2js');
     const x2js = new x2jsrequire();
     const timeoutDuration = 20000;
+
+    const xml = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <tot:ReadRecord>
+                <!--Optional:-->
+                <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
+                <!--Optional:-->
+                <tot:PrimaryKey>${codColigada};${codSistema};${codSentenca}</tot:PrimaryKey>
+                <!--Optional:-->
+                <tot:Contexto>codcoligada=${codColigada};codfilial=1;codsistema=${codSistema};codusuario=${user}</tot:Contexto>
+            </tot:ReadRecord>
+        </soapenv:Body>
+    </soapenv:Envelope>`;
 
     const config = {
         method: 'POST',
@@ -13,6 +28,7 @@ function buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenc
             SOAPAction: `http://www.totvs.com/IwsDataServer/ReadRecord`
         }
     };
+
     const soapRequestWithTimeout = (url, config, xml) => {
         const soapPromise = soapRequest(url, config, xml);
         const timeoutPromise = new Promise((resolve, reject) => {
@@ -33,48 +49,10 @@ function buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenc
             request.end(xml);
         });
     };
-    const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
-    <soapenv:Header/>
-    <soapenv:Body>
-       <tot:ReadRecord>
-          <!--Optional:-->
-          <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
-          <!--Optional:-->
-          <tot:PrimaryKey>${codColigada};${codSistema};${codSentenca}</tot:PrimaryKey>
-          <!--Optional:-->
-          <tot:Contexto>codcoligada=${codColigada};codfilial=${codFilial};codsistema=${codSistema};codusuario=${user}</tot:Contexto>
-       </tot:ReadRecord>
-    </soapenv:Body>
- </soapenv:Envelope>`
- /*`<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
-   <soapenv:Header/>
-   <soapenv:Body>
-      <tot:ReadRecord>
-         <!--Optional:-->
-         <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
-         <!--Optional:-->
-         <tot:PrimaryKey>0;S;RB.PS.IM.010</tot:PrimaryKey>
-         <!--Optional:-->
-         <tot:Contexto>codcoligada=1;codfilial=1;codsistema=s;codusuario=rubeus</tot:Contexto>
-      </tot:ReadRecord>
-   </soapenv:Body>
-</soapenv:Envelope>`*/;
-    /*const myAction = async () => {
-        try {
-          const response = await soapRequestWithTimeout(url, config, xml);
-          if (response.status === 200) {
-            const sentença = JSON.parse(response.Envelope.Body.ReadRecordResponse.ReadRecordResult);
-            return sentença;
-          } else {
-            return 'Erro na solicitação SOAP:' + response.status;
-          }
-        } catch (error) {
-          return 'Erro na solicitação SOAP:' + error;
-        }
-      };*/
+
     const myAction = async () => {
         try {
-            const response = await soapRequestWithTimeout(url, config, xml);
+            const response = await soapRequestWithTimeout(`${url}/wsDataServer/IwsDataServer`, config, xml);
             const json = x2js.xml2js(response);
             const stringXML = json.Envelope.Body.ReadRecordResponse.ReadRecordResult
             const sentence = x2js.xml2js(stringXML).GlbConsSql.GConsSql
--- src/conect-tbc/salvarConsulta.js
+++ src/conect-tbc/salvarConsulta.js
@@ -1,18 +1,50 @@
-function salvarConsulta(url, user, passWord,codSistema,codSentenca,titleSentenca,conteudoSentenca,codFilial) {
+function salvarConsulta(url, user, passWord, codColigada, codSistema, codSentenca, titleSentenca, conteudoSentenca) {
     const https = require('https')
-    const http = require('http'); // Use the 'https' module instead of 'http'
+    const http = require('http');
     const x2jsrequire = require('x2js');
     const x2js = new x2jsrequire();
     const timeoutDuration = 20000;
+
+    const xml = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <tot:SaveRecord>
+            <!--Optional:-->
+            <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
+            <!--Optional:-->
+            <tot:XML><![CDATA[
+                    <GlbConsSql>
+                    <GConsSql>
+                        <CODCOLIGADA>${codColigada}</CODCOLIGADA>
+                        <APLICACAO>${codSistema}</APLICACAO>
+                        <CODSENTENCA>${codSentenca}</CODSENTENCA>
+                        <TITULO>${titleSentenca}</TITULO>
+                        <SENTENCA>${conteudoSentenca}</SENTENCA>  
+                        <DISPONIVELFILTRO>1</DISPONIVELFILTRO>
+                        <DISPONIVELRELATORIO>1</DISPONIVELRELATORIO>
+                        <DISPONIVELVISAO>1</DISPONIVELVISAO>
+                        <DISPONIVELMENU>0</DISPONIVELMENU>
+                        <SEMSEGCOLUNAS>0</SEMSEGCOLUNAS>
+                        <SEMSEGESTENDIDA>0</SEMSEGESTENDIDA>
+                    </GConsSql>
+                        </GlbConsSql>]]></tot:XML>
+            <!--Optional:-->
+            <tot:Contexto>codcoligada=${codColigada};codfilial=1;codsistema=${codSistema};codusuario=${user}</tot:Contexto>
+        </tot:SaveRecord>
+        </soapenv:Body>
+    </soapenv:Envelope>`;
+
 
     const config = {
         method: 'POST',
         headers: {
             Authorization: 'Basic ' + Buffer.from(`${user}:${passWord}`).toString('base64'),
             'Content-Type': 'text/xml',
-            SOAPAction: `${url}/wsDataServer/IwsDataServer`
+            SOAPAction: `http://www.totvs.com/IwsDataServer/SaveRecord`
         }
     };
+
     const soapRequestWithTimeout = (url, config, xml) => {
         const soapPromise = soapRequest(url, config, xml);
         const timeoutPromise = new Promise((resolve, reject) => {
@@ -33,40 +65,12 @@ function salvarConsulta(url, user, passWord,codSistema,codSentenca,titleSentenca
             request.end(xml);
         });
     };
-    const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tot="http://www.totvs.com/">
-    <soapenv:Header/>
-    <soapenv:Body>
-       <tot:SaveRecord>
-          <!--Optional:-->
-          <tot:DataServerName>GlbConsSqlData</tot:DataServerName>
-          <!--Optional:-->
-          <tot:XML><![CDATA[
-             <GlbConsSql>
-               <GConsSql>
-                   <CODCOLIGADA>${codColigada}</CODCOLIGADA>
-                   <APLICACAO>${codSistema}</APLICACAO>
-                   <CODSENTENCA>${codSentenca}</CODSENTENCA>
-                   <TITULO>${titleSentenca}</TITULO>
-                   <SENTENCA>${conteudoSentenca}</SENTENCA>  
-                   <DISPONIVELFILTRO>1</DISPONIVELFILTRO>
-                   <DISPONIVELRELATORIO>1</DISPONIVELRELATORIO>
-                   <DISPONIVELVISAO>1</DISPONIVELVISAO>
-                   <DISPONIVELMENU>0</DISPONIVELMENU>
-                   <SEMSEGCOLUNAS>0</SEMSEGCOLUNAS>
-                   <SEMSEGESTENDIDA>0</SEMSEGESTENDIDA>
-               </GConsSql>
-                </GlbConsSql>]]>
-          </tot:XML>
-          <!--Optional:-->
-          <tot:Contexto>codcoligada=${codColigada};codfilial=${codFilial};codsistema=${codSistema};codusuario=${user}</tot:Contexto>
-       </tot:SaveRecord>
-    </soapenv:Body>
- </soapenv:Envelope>`;
 
     const myAction = async () => {
         try {
-            const response = await soapRequestWithTimeout(url, config, xml);
+            const response = await soapRequestWithTimeout(`${url}/wsDataServer/IwsDataServer`, config, xml);
             const json = x2js.xml2js(response);
+            console.log(xml)
             return json.Envelope.Body
         } catch (error) {
             return 'Erro na solicitação SOAP:' + error
