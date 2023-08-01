adb57a6a82950b74e9a921aff6ae4b4aacf0cf8f
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-08-01 08:57:03 -0300

    criando a rota post

--- api/server.js
+++ api/server.js
@@ -1,26 +1,37 @@
 const valorLiquido = require("../src/conect-tbc/valorLiquido")
 const buscarConsulta = require("../src/conect-tbc/buscarConsulta")
-const express = require("express")
+const express = require("express");
+const salvarConsulta = require("../src/conect-tbc/salvarConsulta");
 
 const app = express();
 app.use(express.json());
 
 app.get("/valorLiquido", async (req, res) => {
-    try{
+    try {
         const { url, user, passWord, idlan, codcoligada } = req.body;
         const response = await valorLiquido(url, user, passWord, idlan, codcoligada);
         return res.status(200).json(response);
-    }catch(erro){
+    } catch (erro) {
         return res.status(500).json(erro)
     }
 });
 app.get("/consulta", async (req, res) => {
     try {
-        const { url, user, passWord,codcoligada,codSistema,codSentenca,codFilial } = req.body;
-        const response = await buscarConsulta(url, user, passWord, codcoligada,codSistema,codSentenca,codFilial);
+        const { url, user, passWord, codColigada, codSistema, codSentenca } = req.body;
+        const response = await buscarConsulta(url, user, passWord, codColigada, codSistema, codSentenca);
         return res.status(200).json(response);
     } catch (error) {
-        return res.status(500).json(erro)
+        return res.status(500).json(`Internal server error: ${error}`)
+    }
+})
+
+app.post("/consulta", async (req, res) => {
+    try {
+        const { url, user, passWord, codColigada, codSistema, codSentenca, titleSentenca, conteudoSentenca } = req.body
+        const response = await salvarConsulta(url, user, passWord, codColigada, codSistema, codSentenca, titleSentenca, conteudoSentenca)
+        return res.status(201).json(response)
+    } catch (error) {
+        return res.status(500).json(`Internal server error: ${error}`)
     }
 })
 
